65613f8ee9c21ccf4db438ddfd8538700de0b04d
Michał Seta <djiamnot@gmail.com>
2018-05-04 09:16:04 -0400

    remove unused classes

--- register_types.cpp
+++ register_types.cpp
@@ -1,18 +1,12 @@
 /* register_types.cpp */
 
 #include "register_types.h"
-#include "osc_transmitter.h"
-#include "osc_listener.h"
 
 #include "OSCreceiver.h"
 #include "OSCmessage.h"
 #include "OSCsender.h"
 
 void register_gdosc_types() {
-	// old classes
-	ClassDB::register_class<OSCtransmitter>();
-	ClassDB::register_class<OSCListener>();
-	// new classes
 	ClassDB::register_class<OSCreceiver>();
 	ClassDB::register_class<OSCmessage>();
 	ClassDB::register_class<OSCsender>();
