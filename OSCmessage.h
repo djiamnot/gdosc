4a2e6667c18db1103b0c0fd946478549d2ec8ef0
Michał Seta <djiamnot@gmail.com>
2018-05-05 12:46:21 -0400

    fix some whitespace

--- OSCmessage.h
+++ OSCmessage.h
@@ -11,15 +11,15 @@ class OSCmessage: public Resource {
 	GDCLASS(OSCmessage, Resource);
 	RES_BASE_EXTENSION("oscmessage");
 
-public:
-	
+ public:
+
 	OSCmessage();
 	OSCmessage(const osc::ReceivedMessage &m, const IpEndpointName &remoteEndpoint);
 	~OSCmessage();
-	
+
 	// getter
 	_FORCE_INLINE_ const bool& is_valid() const { return _valid; }
-	
+
 	// getter, exposed in gdscript
 	_FORCE_INLINE_ bool empty() const { return _arg_num < 1; }
 	_FORCE_INLINE_ const String& ip() const { return _ip; }
@@ -28,17 +28,17 @@ public:
 	_FORCE_INLINE_ const String& typetag() const { return _typetag; }
 	_FORCE_INLINE_ const int& arg_num() const { return _arg_num; }
 	_FORCE_INLINE_ const Variant& arg(int p_idx) const { return _arguments[p_idx]; }
-	
+
 	// operators
 	void copy( const OSCmessage& src );
-	
+
 protected:
-	
+
 	static void _bind_methods();
 	const Array& arguments() const { return _arguments; }
-	
+
 private:
-	
+
 	bool _valid;
 	String _ip;
 	int _port;
@@ -46,7 +46,7 @@ private:
 	String _typetag;
 	int _arg_num;
 	Array _arguments;
-	
+
 };
 
 
